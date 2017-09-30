829a1b34d4ea9e42ed06e6be8788ee477d7da3c7
Andy Noblet <andrewbnoblet@gmail.com>
2017-09-30 22:11:43 +0000

    Update

--- Application.php
+++ Application.php
@@ -1,3 +1,14 @@
 <?php
+    // Set base
+    define("BASE", __DIR__);
+
+    // Set Path
+    $Name = str_replace($_SERVER['DOCUMENT_ROOT'], null, $_SERVER['SCRIPT_FILENAME']);
+    $Parts = explode("/", $Name);
+    array_pop($Parts);
+    $Path = implode("/", $Parts);
+    $Path = $Path . "/";
+    define("PATH", $Path);
+
     require_once("Library/autoload.php");
     Application::Start();
