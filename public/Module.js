a0f58be0573bcd9a660436d3af319692a273c450
Andy Noblet <andrewbnoblet@gmail.com>
2017-10-17 15:37:40 -0400

    Update

--- public/Module.js
+++ public/Module.js
@@ -1,9 +1,20 @@
 (function (app) {
-    app.AppModule = function AppModule() {}
+    app.AppModule = function AppModule() {
+    }
 
     app.AppModule.annotations = [
         new ng.core.NgModule({
-            imports: [ng.platformBrowser.BrowserModule, ng.platformBrowser.animations.BrowserAnimationsModule, ng.material.MatToolbarModule, ng.material.MatSidenavModule, ng.material.MatCardModule, ng.material.MatExpansionModule, ng.material.MatButtonModule, ng['flex-layout'].FlexLayoutModule],
+            imports: [
+                ng.platformBrowser.BrowserModule,
+                ng.platformBrowser.animations.BrowserAnimationsModule,
+                ng.material.MatToolbarModule,
+                ng.material.MatSidenavModule,
+                ng.material.MatCardModule,
+                ng.material.MatExpansionModule,
+                ng.material.MatButtonModule,
+                ng.material.MatMenuModule,
+                ng['flex-layout'].FlexLayoutModule
+            ],
             declarations: [app.AppComponent],
             bootstrap: [app.AppComponent]
         })
